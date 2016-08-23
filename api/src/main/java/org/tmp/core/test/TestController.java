a944a3c94637afd787d6ab58cb160ee28a218c1c
Passarinho <passarinho4@gmail.com>
2016-08-23 21:06:41 +0200

    fix

--- api/src/main/java/org/tmp/core/test/TestController.java
+++ api/src/main/java/org/tmp/core/test/TestController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by mf57 on 15.08.2016.
  */
@@ -17,10 +21,15 @@ public class TestController {
     @Autowired
     private CommandBus commandBus;
 
-    @RequestMapping(value = "/api/tests", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/tests", method = RequestMethod.POST)
     public void create(@RequestBody TestRequest body) {
         System.out.println("Jestem w controllerze");
         CreateTestCommand command = new CreateTestCommand(body.getName(), body.getMessage());
         commandBus.dispatch(new GenericCommandMessage<Object>(command));
+    }
+
+    @RequestMapping(value = "/api/tests", method = RequestMethod.GET)
+    public List<TestRequest> getAllTests() {
+        return Arrays.asList(new TestRequest("CORE", "HELLO WORLD"));
     }
 }
